8af319f449c40feefbc105170b87307317f70f10
chitti239 <sharinis239@gmail.com>
2025-04-11 13:44:41 +0530

    Completed the assignment

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,21 @@
 // write button card here
+import React from 'react'
+
+const button = () => {
+  return (
+    <div>
+      <button style={{
+      padding: "10px 20px",
+      backgroundColor: "black",
+      color: "white",
+      border: "none",
+      borderRadius: "5px",
+      cursor: "pointer",
+    }}>
+      View Product
+    </button>
+    </div>
+  )
+}
+
+export default button
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,32 @@
 // write product card here
+import React from 'react'
+import Button from './button';
+
+const productcard = () => {
+    const productImage = "https://i.pinimg.com/736x/c2/dc/e1/c2dce1bffd0945f04281aa207dc0c913.jpg";
+    const product = "Cozy Winter Knit";
+    const price = "$99.9";
+  return (
+    <div style={{border: "3px solid #ddd",
+        borderRadius: "8px",
+        maxWidth: "300px",
+        padding: "16px",
+        textAlign: "center",
+        margin: "20px auto",
+        fontFamily: "Arial, sans-serif",
+        backgroundColor:"white"
+        }}>
+      <img src={productImage} alt="product" style={{
+        borderRadius: "5%",
+        width: "300px",
+        height: "500px",
+        marginBottom: "16px"
+      }}/>
+      <p style={{color:'black'}}>{product}</p>
+      <p style={{color:'black'}}>{price}</p>
+      <Button/>
+    </div>
+  )
+}
+
+export default productcard
